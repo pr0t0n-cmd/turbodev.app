a705a5b5b1f1b410e777fa0fe274e79623485f9a
j0-maker <giorgio.bruzzi1@gmail.com>
2023-07-29 00:43:10 +0200

    First Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '$lib/styles/global.css';
+</script>
+
+<div>
+	<header>
+		<!-- <Navbar /> -->
+	</header>
+	<main>
+		<slot />
+	</main>
+	<footer>
+		<!-- <Footer /> -->
+	</footer>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<div class="content">
+    <div>
+        <h1>
+            Turbodev.app
+        </h1>
+        <p>
+            Web App, Ecommerce, Mobile
+        </p>
+    </div>
+</div>
+
+
+<style>
+    h1,
+    p {
+        text-align: center;
+    }
+    h1 {
+        font-size: 4rem;
+        margin-bottom: 2.5rem;
+    }
+    p {
+        font-size: 2.5rem;
+    }
+    .content {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
